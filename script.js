4151dc395b7b651a84aa181e87f9cc3b381b28ab
kelshardy <14024@coderacademy.edu.au>
2023-08-15 20:51:42 +1000

    Add basic JS to page

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * This is a function.
+ * @param {*} someArgument - Some data given to the function.
+ */
+function someFunction(someArgument) {
+    console.log("This was given to the function:" + someArgument);
+}
+
+// The square brackets are an array
+// someFunction([1, 2, "banana"]);
